c380604ab72ea6a9e6c4466eccf577a72059d0e5
neak bruno <brunosneak@gmail.com>
2020-10-08 12:19:17 +0200

    init components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { CvComponent } from './cv/cv.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjetsComponent } from './projets/projets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    HeaderComponent,
+    CvComponent,
+    ContactComponent,
+    ProjetsComponent
   ],
   imports: [
     BrowserModule,
