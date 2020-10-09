12670b3c70010993a8b48671834310c81bbe8f9e
neak bruno <brunosneak@gmail.com>
2020-10-09 09:49:15 +0200

    display page cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { CvComponent } from './cv/cv.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjetsComponent } from './projets/projets.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+import { CvComponent } from "./cv/cv.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ProjetsComponent } from "./projets/projets.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    HomeComponent,
-    HeaderComponent,
-    CvComponent,
-    ContactComponent,
-    ProjetsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavComponent,
+		HomeComponent,
+		HeaderComponent,
+		CvComponent,
+		ContactComponent,
+		ProjetsComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
