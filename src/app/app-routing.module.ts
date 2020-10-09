1978801697c11ebea7eb6dbe613979f3cffb2722
neak bruno <brunosneak@gmail.com>
2020-10-09 12:32:29 +0200

    animation nav menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+import { CvComponent } from "./cv/cv.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "", component: HomeComponent },
+	{ path: "contact", component: ContactComponent },
+	{ path: "cv", component: CvComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+	selector: "app-nav",
+	templateUrl: "./nav.component.html",
+	styleUrls: ["./nav.component.scss"],
 })
 export class NavComponent implements OnInit {
+	public navStatus: boolean = false;
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	animButton() {
+		this.navStatus = !this.navStatus;
+		console.log(this.navStatus);
+	}
 }
